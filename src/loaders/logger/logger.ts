59ca33e3dc9323b7b5388654c3c778255e6b99ae
1z0ldyck <lucase2040@gmail.com>
2022-09-23 00:58:44 -0300

    remove comments

--- src/loaders/logger/logger.ts
+++ src/loaders/logger/logger.ts
@@ -4,10 +4,6 @@ export default winston.createLogger({
   level: 'info',
   format: winston.format.simple(),
   transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
     new winston.transports.Console(),
   ],
 });
